01d9a8bf7476e73e187db169df96b86c022a778f
tanaka343 <mnhyodo.yc@kaien-t3.net>
2025-11-10 12:15:09 +0900

    不要コード、未使用インポートの削除

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import pandas as pd
-from pathlib import Path
-from selenium.webdriver.remote.webelement import WebElement
-from utils.input_handlers import get_user_input,split_address
+from utils.input_handlers import split_address
 from utils.data_extractors import scrape_data
-from results.output import copy_template_sheet,write_to_excel
-import sys 
-from utils.selenium_helpers import get_site
-from flask import Flask,render_template,redirect,request,send_from_directory,session,send_file,flash
-import os
+from flask import Flask,render_template,redirect,request,session,send_file,flash
 import io
 
 
@@ -64,8 +52,6 @@ def search():
     df=pd.read_json(json_data,orient='split')
   else:
     try:
-      # facility_list = main(input_location)
-      # num=len(facility_list)
       df = main(input_location)
       session['my_dataframe']=df.to_json(orient='split')
       session['last_input']=input_location
@@ -87,8 +73,6 @@ def search():
     return render_template('/output.html',facility_list=facility_list,input_location=input_location)
   else:
     return redirect('/')
-# base_dir = os.path.dirname(__file__)
-# DOWNLOAD_DIR_PATH = os.path.join(base_dir,'自動化アプリ出力')
 
 
 XLSX_MIMETYPE='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
@@ -130,7 +114,7 @@ def download():
 
     
     output_buffer.seek(0)
-    # session.pop('my_dataframe', None) 
+    
   
     return send_file(output_buffer,  \
         mimetype=file_mimetype,as_attachment = True, download_name = downloadFileName)
@@ -149,35 +133,17 @@ def main(input_location):
   -Excelファイルの指定シートに抽出した情報を書き込み
   
   """
-  # try:
-    # input_location,excel_path =get_user_input()
   prefecture,city_name =split_address(input_location)
   facility_data_list =scrape_data(input_location,prefecture,city_name)
   df = pd.DataFrame(facility_data_list) 
-  # excel_path,sheet_name =copy_template_sheet(excel_path,city_name)   
-  # write_to_excel(excel_path,df,sheet_name)
-
+  
   print(df)
   print(f"\033[0m\033[32m\033[1m処理が正常に完了しました。\033[0m")
   return df
-  # except ValueError :
-  #   print('入力形式が正しくありません。')
-  #   sys.exit(1)
-  # except PermissionError:
-  #   print('excelファイルを閉じてください。')
-  #   sys.exit(1)
-
-  # except TimeoutException as e:
-  #   print(f"エラーが発生:{e}")
-  #   sys.exit(1)
-  # except Exception:
-  #   print('予期せぬエラーが発生しました。')
-  #   import traceback
-  #   traceback.print_exc()
+  
 
   
 
 if __name__ == "__main__":
-  #  main()
   app.run()
  
